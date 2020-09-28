f714263157fa6624fb06cc78a1ac30af46b8ce75
macz <macolinares@gmail.com>
2020-09-28 09:11:06 -0600

    Errores ortograficos en paginas de acerca

--- resources/views/about/index.blade.php
+++ resources/views/about/index.blade.php
@@ -16,18 +16,11 @@
             <div class="col-lg-7">
                 <div class="about-details mb-50">
                     <p class="pear1">Expertos en Tecnologías de la Información</p>
-                    <p class="pear2">Bitsis S.A. nace con el objetivo de proveer sistemas de información de alta calidad,
-                        personalizado para nuestros clientes, así como la asesoría a las pequeñas y medianas empresas en todas la región 
-                        del territorio nacional. A pesar del gran crecimiento tecnológico en la región, muchas empresas carece de tecnologías 
-                        de la inforcación para el correcto desarroollo y control de sus operaciones.
+                    <p class="pear2">Bitsis S.A. nace con el objetivo de proveer sistemas de información de alta calidad, personalizado para nuestros clientes, así como la asesoría a las pequeñas y medianas empresas en todas las regiones del territorio nacional. A pesar del gran crecimiento tecnológico en la región, muchas empresas carecen de tecnologías de la información para el correcto desarrollo y control de sus operaciones.
                     </p>
-                    <p class="pear2">La gran mayoría de empresas en la región, tienen origen en emprendimientos familiares. Una característica
-                        de estas empresa es que no cuentan con sistemas de control de invetarios, ventas, contabilidad, recurso humano, financiero 
-                        etc. Razón que precede el estancamiento del crecimiento de estas empresas.
+                    <p class="pear2">La gran mayoría de empresas en la región, tienen origen en emprendimientos familiares. Una característica de estas empresas es que no cuentan con sistemas de control de inventarios, ventas, contabilidad, recurso humano, financiero etc. Razón que precede el estancamiento del crecimiento de estas empresas.
                     </p>
-                    <p class="pear2">Bitsis S.A. tiene como uno de sus objetivos, brindar asesoría de alto nivel y provisión del software 
-                        necesario a todas éstas empresas, con el fin de proveer sistemas de información que les permitan simplificar el control 
-                        y la gestión, incrementando de esta manera sus ganancias y mejorando las bases para su crecimiento.
+                    <p class="pear2">Bitsis S.A. tiene como uno de sus objetivos, brindar asesoría de alto nivel y provisión del software necesario a todas éstas empresas, con el fin de proveer sistemas de información que les permitan simplificar el control y la gestión, incrementando de esta manera sus ganancias y mejorando las bases para su crecimiento.
                     </p>
                 </div>
             </div>
@@ -39,8 +32,7 @@
             <div class="col-lg-7">
                 <div class="about-details">
                     <p class="pear1">Comprometidos con nuestra misión</p>
-                    <p class="pear2">Ofrecer soluciones tecnológicas de alta calidad, construidas cobre las más sólidas bases del conocimiento, logrando 
-                        proveer servicios y productos a la medida, excepcionales y financieramente competitivos."
+                    <p class="pear2">Ofrecer soluciones tecnológicas de alta calidad, construidas sobre las más sólidas bases del conocimiento, logrando proveer servicios y productos a la medida, excepcionales y financieramente competitivos.
                     </p>
                 </div>
             </div>
@@ -53,7 +45,7 @@
                 <div class="about-details">
                     <p class="pear1">Nuestra visión</p>
                     <p class="pear2">Usar nuestra ilimitada pasión por la tecnología, contenidos y servicios para otorgar soluciones y 
-                        simplicidad innovadora, posicionándonos como una empresa de alto valor en el mercado Latinoamericano."
+                        simplicidad innovadora, posicionándonos como una empresa de alto valor en el mercado Latinoamericano.
                     </p>
                 </div>
             </div>
